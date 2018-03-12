311cae56f8547b77b86e95fa2006dcacf33993b3
dhayakawa <dhayakawa@nelsonjameson.com>
2018-03-12 17:28:21 -0500

    updates

--- src/SpringIntoActionServiceProvider.php
+++ src/SpringIntoActionServiceProvider.php
@@ -38,9 +38,7 @@
             $this->publishes([__DIR__ . '/resources' => base_path('resources/')], 'resources');
             $this->publishes([__DIR__ . '/public' => base_path('public/')], 'public');
             $this->publishes([__DIR__ . '/Models' => app_path('Models')], 'models');
-            $this->publishes([__DIR__ . '/Menu' => app_path('Menu')], 'menu');
             $this->publishes([__DIR__ . '/migrations' => app_path('migrations')], 'migrations');
-            //$this->publishes([__DIR__ . '/Notifications' => app_path('Notifications')], 'notifications');
             $this->publishes([__DIR__ . '/webpack.mix.js' => base_path('webpack.mix.js')], 'webpack');
 
             // If routes file has been published, load routes from the published file
@@ -59,11 +57,7 @@
             $this->publishes([
                 __DIR__ . '/resources/views/vendor/springintoaction' => resource_path('views/vendor/springintoaction'),
             ]);
-            // Loading dynamic menu when calling the view
-            //View::composer('springintoaction::layout.mainsidebar', 'Dhayakawa\SpringIntoAction\ViewComposers\MenuComposer');
 
-            // For datatables locales
-            //View::composer('springintoaction::load.datatables', 'Dhayakawa\SpringIntoAction\ViewComposers\DatatablesComposer');
         }
 
         /**
@@ -76,6 +70,5 @@
             $this->mergeConfigFrom(__DIR__ . '/config/springintoaction/app.php', 'springintoaction.app');
             $this->mergeConfigFrom(__DIR__ . '/config/springintoaction/signup.php', 'springintoaction.signup');
             $this->mergeConfigFrom(__DIR__ . '/config/springintoaction/auth.php', 'springintoaction.auth');
-            $this->mergeConfigFrom(__DIR__ . '/config/springintoaction/menu.php', 'springintoaction.menu');
         }
     }
