c5111f0bcbf1febe5c062975e2270d8a01928e36
dhayakawa <dhayakawa@nelsonjameson.com>
2018-09-28 17:40:14 -0500

    removing login data

--- src/config/springintoaction/onedrive.php
+++ src/config/springintoaction/onedrive.php
@@ -1,18 +1,11 @@
 <?php
 
 return [
-	'vagrant' => [
-		'client_id'         => '2d748eb3-8814-440b-a1ff-2ced2304874e',
-		'client_secret'     => 'qpdnokFTIA2~_#kQFZ4043~',
-		'callback_uri'      => 'https://localhost:443/onedrive_callback',
+	'site' => [
+		'client_id'         => '',
+		'client_secret'     => '',
+		'callback_uri'      => '',
 		'skydrive_base_url' => 'https://apis.live.net/v5.0/',
 		'token_store'       => 'onedrive_oauth_tokens'
 	],
-	'woodlands' => [
-		'client_id'         => 'ec280603-df38-4535-9aa6-56633b64eeca',
-		'client_secret'     => 'pnggIDM5:;%nnkQHQS8412}',
-		'callback_uri'      => 'https://springintoaction.woodlandschurch.org/onedrive_callback',
-		'skydrive_base_url' => 'https://apis.live.net/v5.0/',
-		'token_store'       => 'onedrive_oauth_tokens'
-	]
 ];
