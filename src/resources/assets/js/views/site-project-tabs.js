16bdaeb19516d310c49465689b528fcbfda029a6
dhayakawa <dhayakawa@nelsonjameson.com>
2019-04-14 19:50:24 -0500

    another try at fixing duplicated events and data

--- src/resources/assets/js/views/site-project-tabs.js
+++ src/resources/assets/js/views/site-project-tabs.js
@@ -363,11 +363,12 @@
                     //initialize your views here
                     _log('App.Views.SiteProjectTabs.fetchIfNewProjectID.event', 'tab collections fetch promise done');
                     self.mainApp.$('h3.box-title small').html(self.model.get('ProjectDescription'));
-                    self.projectLeadsView.render();
-                    self.projectBudgetView.render();
-                    self.projectContactsView.render();
-                    self.projectVolunteersView.render();
-                    self.projectAttachmentsView.render();
+                    // DO NOT RE-Render or duplicate data and events start up
+                    // self.projectLeadsView.render();
+                    // self.projectBudgetView.render();
+                    // self.projectContactsView.render();
+                    // self.projectVolunteersView.render();
+                    // self.projectAttachmentsView.render();
                     window.ajaxWaiting('remove', '.tab-content.backgrid-wrapper');
                 });
                 _log('App.Views.SiteProjectTabs.fetchIfNewProjectID.event', 'setting data-project-id to ' + this.model.get('ProjectID') + ' on', this.$el);
