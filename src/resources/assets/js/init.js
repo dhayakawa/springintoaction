e2e121fdeed386185c31626b1d4d224076215ab6
dhayakawa <dhayakawa@nelsonjameson.com>
2019-11-19 15:45:40 -0600

    removing filestack

--- src/resources/assets/js/init.js
+++ src/resources/assets/js/init.js
@@ -68,67 +68,7 @@ window.App = {
     CellEditors: {}
 };
 
-const fileStackApiKey = 'Ana2T1xQQ76feCTmPqLlSz';
-//let fileStackClient = filestack.init(fileStackApiKey);
 
-let fileStackCombOptions = {
-    "displayMode": "inline",
-    "container": ".picker-content",
-    "maxFiles": 5,
-    "accept": [
-        "image/jpeg",
-        "image/jpg",
-        "image/png",
-        "image/bmp",
-        "image/gif",
-        "application/pdf"
-    ],
-    "storeTo": {
-        "container": "devportal-customers-assets",
-        "path": "user-uploads/",
-        "region": "us-east-1"
-    },
-    "fromSources": [
-        "local_file_system"
-    ],
-    "uploadInBackground": false
-};
-/**
- * let fileStackComboPicker = fileStackClient.picker(fileStackComboOptions);
- * fileStackComboPicker.open();
- */
-
-
-let fileStackDragDropOptions = {
-    "displayMode": "dropPane",
-    "container": ".picker-content",
-    "maxFiles": 4,
-    "accept": [
-        "image/jpeg",
-        "image/jpg",
-        "image/png",
-        "image/bmp",
-        "image/gif",
-        "application/pdf"
-    ],
-    "storeTo": {
-        "container": "devportal-customers-assets",
-        "path": "user-uploads/",
-        "region": "us-east-1"
-    },
-    "fromSources": [
-        "local_file_system"
-    ],
-    "uploadInBackground": false,
-    "dropPane": {
-        "overlay": false
-    }
-};
-/*
- * Just Drag N Drop
- * let fileStackDragDropPicker = fileStackClient.picker(fileStackDragDropOptions);
- * fileStackDragDropPicker.open();
-*/
 
 /**
  * Needed to fix the select so when it triggers a backgrid:edited the model has been flagged as changed
